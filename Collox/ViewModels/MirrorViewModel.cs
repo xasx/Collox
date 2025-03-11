246eceb6ad32c4bf766bc2658914b78f6f2f39ec
Andreas Schöneck <as.maps@gmail.com>
2025-03-11 01:21:42 +0100

    Enhance message filtering in MirrorViewModel and UI
    Updated MirrorViewModel to manage message contexts with new properties for filtering. Introduced FilteredMessages and Contexts collections, and modified the Receive method to handle context-based message addition. Added a CommandBar in MirrorPage.xaml for context selection, and updated ListView to bind to FilteredMessages for displaying relevant messages. Changed TimeSpanToFriendlyConverter resource key for clarity.

--- Collox/ViewModels/MirrorViewModel.cs
+++ Collox/ViewModels/MirrorViewModel.cs
@@ -1,20 +1,56 @@
 ﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
 using CommunityToolkit.Mvvm.Messaging;
+using NLog.Filters;
 
 namespace Collox.ViewModels;
 
 public partial class MirrorViewModel : ObservableRecipient, IRecipient<TextSubmittedMessage>
 {
+    private static readonly string All = "All";
+
     [ObservableProperty] public partial ObservableCollection<TextColloxMessage> Messages { get; set; } = [];
+
+    [ObservableProperty] public partial ObservableCollection<TextColloxMessage> FilteredMessages { get; set; } = [];
+
+    [ObservableProperty] public partial string SelectedContext { get; set; } = All;
+
+    [ObservableProperty] public partial ObservableCollection<string> Contexts { get; set; } = [All];
 
     public MirrorViewModel()
     {
-        WeakReferenceMessenger.Default.Register<TextSubmittedMessage>(this);
+        WeakReferenceMessenger.Default.Register(this);
     }
 
     public void Receive(TextSubmittedMessage message)
     {
         Messages.Add(message.Value);
+        if (!Contexts.Contains(message.Value.Context))
+        {
+            Contexts.Add(message.Value.Context);
+        }
+        if (SelectedContext == All || SelectedContext == message.Value.Context)
+        {
+            FilteredMessages.Add(message.Value);
+        }
     }
+
+    public void FilterMessages()
+    {
+        FilteredMessages.Clear();
+        if (SelectedContext == All)
+        {
+            FilteredMessages.AddRange(Messages);
+        }
+        else
+        {
+            FilteredMessages.AddRange(Messages.Where(m => m.Context == SelectedContext));
+        }
+    }
+
+    partial void OnSelectedContextChanged(string value)
+    {
+        FilterMessages();
+    }
+
 }
