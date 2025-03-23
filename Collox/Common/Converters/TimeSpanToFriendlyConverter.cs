5b0a1eeaf404ed185616c6351c31a2daea25187f
Andreas Schöneck <as.maps@gmail.com>
2025-03-24 00:57:51 +0100

    Enhance localization for friendly time
    Updated TimeSpanToFriendlyConverter to support new string properties for various time intervals. Added German translations in Collox.de-DE.xlf and created Helpers.resw for resource strings. Updated XAML files to link the converter with localization attributes. Included schema definition in the new Helpers.resw for clarity and maintainability.

--- Collox/Common/Converters/TimeSpanToFriendlyConverter.cs
+++ Collox/Common/Converters/TimeSpanToFriendlyConverter.cs
@@ -1,45 +1,54 @@
 ﻿using Microsoft.UI.Xaml.Data;
+using Microsoft.Windows.ApplicationModel.Resources;
 
 namespace Collox.Common.Converters;
 
 public partial class TimeSpanToFriendlyConverter : IValueConverter
 {
+    public string SecondsAgo { get; set; } = "{0} seconds ago";
+    public string MinutesAgo { get; set; } = "{0} minutes ago";
+    public string HoursAgo { get; set; } = "{0} hours ago";
+    public string DaysAgo { get; set; } = "{0} days ago";
+    public string WeeksAgo { get; set; } = "{0} weeks ago";
+    public string MonthsAgo { get; set; } = "{0} months ago";
+    public string YearsAgo { get; set; } = "{0} years ago";
+
     public object Convert(object value, Type targetType, object parameter, string language)
     {
         var timeSpan = (TimeSpan)value;
         if (timeSpan.TotalSeconds < 60)
         {
-            return $"{timeSpan.Seconds} seconds ago";
+            return string.Format(SecondsAgo, timeSpan.Seconds);
         }
 
         if (timeSpan.TotalMinutes < 60)
         {
-            return $"{timeSpan.Minutes} minutes ago";
+            return string.Format(MinutesAgo, timeSpan.Minutes);
         }
 
         if (timeSpan.TotalHours < 24)
         {
-            return $"{timeSpan.Hours} hours ago";
+            return string.Format(HoursAgo, timeSpan.Hours);
         }
 
         if (timeSpan.TotalDays < 7)
         {
-            return $"{timeSpan.Days} days ago";
+            return string.Format(DaysAgo, timeSpan.Days);
         }
 
         if (timeSpan.TotalDays < 30)
         {
-            return $"{timeSpan.Days / 7} weeks ago";
+            return string.Format(WeeksAgo, timeSpan.Days / 7);
         }
 
         if (timeSpan.TotalDays < 365)
         {
-            return $"{timeSpan.Days / 30} months ago";
+            return string.Format(MonthsAgo, timeSpan.Days / 30);
         }
 
         if (timeSpan.TotalDays >= 365)
         {
-            return $"{timeSpan.Days / 365} years ago";
+            return string.Format(YearsAgo, timeSpan.Days / 365);
         }
 
         return $"{value}";
