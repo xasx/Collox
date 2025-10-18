840bc6c32c6fa90720f8da8fb0d2b3ceabfe2784
Andreas Schöneck <as.maps@gmail.com>
2025-10-18 23:15:50 +0200

    Add SystemInformationPage and improve UI animations
    Introduced a new `SystemInformationPage` to display runtime,
    application, system information, and dependencies. Moved
    related content from `HomeLandingPage` to improve modularity.
    
    Updated `HomeLandingPage` with animations for smoother
    transitions and enhanced user experience. Added a
    `Page_Loaded` event to trigger animations.
    
    Modified `Collox.csproj` to include `SystemInformationPage`
    and updated dependencies:
    - Added: `Microsoft.Windows.CsWinRT`, `Microsoft.WindowsAppSDK`,
      `Microsoft.Xaml.Behaviors.WinUI.Managed`, etc.
    - Updated: `WinUIEx` to v2.9.0.
    - Removed: `System.Net.Http`, `System.Private.Uri`, and others.
    
    Improved overall project structure and user interface.

--- Collox/Views/HomeLandingPage.xaml.cs
+++ Collox/Views/HomeLandingPage.xaml.cs
@@ -4,9 +4,14 @@ public sealed partial class HomeLandingPage : Page
 {
     public HomeLandingPage()
     {
-        DataContext = App.GetService<HomeLandingViewModel>();
+        //DataContext = App.GetService<HomeLandingViewModel>();
         InitializeComponent();
     }
 
-    public HomeLandingViewModel ViewModel => DataContext as HomeLandingViewModel;
+    // public HomeLandingViewModel ViewModel => DataContext as HomeLandingViewModel;
+
+    private void Page_Loaded(object sender, RoutedEventArgs e)
+    {
+        PageLoadStoryboard.Begin();
+    }
 }
